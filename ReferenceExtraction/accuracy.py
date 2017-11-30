c1f7967201ce1843df8a2770b8be1852b1d44889
avinabsaha <avinabsaha95@gmail.com>
2017-11-30 16:18:37 +0000

    Adding resources

---
+++ ReferenceExtraction/accuracy.py
@@ -0,0 +1,31 @@
+import sys
+ 
+#usage : python accuracy.py <file>
+ 
+f1 = open(sys.argv[1])
+ 
+count = 0
+true = 0
+false = 0
+sentences = 0
+for line in f1:
+        if line == '\n' or line.split()==[]:
+                sentences+=1
+                continue
+        count = count + 1
+        x = line.strip().split()
+        if(x[-1]==x[-2]):
+                true=true+1
+        else:
+                false = false+1
+ 
+#print '#true', '#false', '#total_words', '#sentences'
+print '#Correctly tagged words:', true
+print '#Incorrectly tagged words:', false
+print '#Total words', count
+
+#print true, false, count, sentences
+ 
+print 'Accuracy = ', (100.0*true/count)
+ 
+f1.close()
